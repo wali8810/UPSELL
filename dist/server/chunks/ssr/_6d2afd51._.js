3efa247de20bc0ab95798e4d36c5eafac5dbc409
Walisson Conta pessoal <walissoncontapessoal@gmail.com>
2025-09-06 23:23:05 +0000

    coloca essa imagem na div de garantia: https://imgur.com/3nS8KCV

--- dist/server/chunks/ssr/_6d2afd51._.js
+++ dist/server/chunks/ssr/_6d2afd51._.js
@@ -2091,7 +2091,7 @@ function Guarantee() {
                     className: "flex flex-col items-center mb-12",
                     children: [
                         /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$image$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["default"], {
-                            src: "https://imgur.com/kdkxPjJ.png",
+                            src: "https://imgur.com/3nS8KCV.png",
                             alt: "Selo de Garantia de 30 dias",
                             width: 250,
                             height: 250,
--- src/components/landing/Guarantee.tsx
+++ src/components/landing/Guarantee.tsx
@@ -7,7 +7,7 @@ export function Guarantee() {
         <div className="container mx-auto px-4 max-w-4xl text-center">
             <div className="flex flex-col items-center mb-12">
                 <Image 
-                    src="https://imgur.com/kdkxPjJ.png"
+                    src="https://imgur.com/3nS8KCV.png"
                     alt="Selo de Garantia de 30 dias"
                     width={250}
                     height={250}
