27eb70e7b0c39f08d9ca8ce98cd134537e62abd9
Walisson Conta pessoal <walissoncontapessoal@gmail.com>
2025-09-07 20:24:04 +0000

    coloque a div de bonus perto da linha pois esta muito longe

--- dist/server/chunks/ssr/_f0909c51._.js
+++ dist/server/chunks/ssr/_f0909c51._.js
@@ -61,7 +61,7 @@ const bonusData = [
 ];
 function Bonuses() {
     return /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("section", {
-        className: "pb-8 bg-white",
+        className: "bg-white pt-8 pb-8",
         children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
             className: "container mx-auto px-4",
             children: [
