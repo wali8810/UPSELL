060f72c03c59bc8b13793daffa98e38b75382d4d
Walisson Conta pessoal <walissoncontapessoal@gmail.com>
2025-09-07 19:36:23 +0000

    linha branca (_for element div_)

--- dist/server/chunks/ssr/_6d2afd51._.js
+++ dist/server/chunks/ssr/_6d2afd51._.js
@@ -1949,7 +1949,7 @@ function Faq() {
                     className: "w-full bg-black text-yellow-400",
                     children: faqItems.map((item, index)=>/*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$src$2f$components$2f$ui$2f$accordion$2e$tsx__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["AccordionItem"], {
                             value: `item-${index}`,
-                            className: index === faqItems.length - 1 ? "border-b-0" : "border-b border-yellow-400/30",
+                            className: index === faqItems.length - 1 ? "border-b-0" : "border-b border-white/30",
                             children: [
                                 /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$src$2f$components$2f$ui$2f$accordion$2e$tsx__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["AccordionTrigger"], {
                                     className: "text-lg text-left hover:no-underline py-6",
