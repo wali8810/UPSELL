b2699b1de553eeb952cadb81b889ba73fb315d4e
Walisson Conta pessoal <walissoncontapessoal@gmail.com>
2025-09-08 00:04:36 +0000

    a imagem que esta na seção de "Do zero ao seu primeiro cartão decorativo

--- src/components/landing/ForWhom.tsx
+++ src/components/landing/ForWhom.tsx
@@ -63,7 +63,7 @@ export function ForWhom() {
                 </p>
                 <div className="mt-8 flex justify-center">
                     <Image
-                        src="https://imgur.com/mva0uTH.png"
+                        src="https://imgur.com/pXzH23f.png"
                         alt="Mockup do curso e bônus"
                         width={608}
                         height={405}
