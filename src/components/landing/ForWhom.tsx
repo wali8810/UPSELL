482fc772fae9a15c0451f7ba7726bd6864454d60
Walisson Conta pessoal <walissoncontapessoal@gmail.com>
2025-09-13 04:06:45 +0000

    coloca essa: https://imgur.com/b4b5pi7 (_for element img_)

--- src/components/landing/ForWhom.tsx
+++ src/components/landing/ForWhom.tsx
@@ -59,7 +59,7 @@ export function ForWhom() {
                 </h2>
                 <div className="mt-8 flex justify-center">
                     <Image
-                        src="https://imgur.com/051Veff.png"
+                        src="https://imgur.com/b4b5pi7.png"
                         alt="Mockup do curso e bônus"
                         width={608}
                         height={405}
