263705b1c0620afe825c9f00832432f6ae7f4d41
Walisson Conta pessoal <walissoncontapessoal@gmail.com>
2025-09-24 00:25:30 +0000

    exporta a pagina completa junto o estilo pos tentei exportar antes e não

--- next.config.mjs
+++ next.config.mjs
@@ -1,36 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  distDir: 'out',
   reactStrictMode: true,
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.imgur.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'imgur.com',
-        port: '',
-        pathname: '/**',
-      }
-    ],
   },
 };
 
