ca4a1350ecd47c9c88e8177753759f18972abf7b
Walisson Conta pessoal <walissoncontapessoal@gmail.com>
2025-09-24 02:07:27 +0000

    Prompt para arrumar o projeto e preparar deploy na Netlify
    Imagine que

--- dist/static/chunks/src_app_layout_tsx_c0237562._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_5ce76384._.js",
-  "static/chunks/src_1246dabe._.js",
-  "static/chunks/src_app_globals_b805903d.css"
-],
-    source: "dynamic"
-});
--- dist/static/chunks/src_app_page_tsx_97b31e30._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_components_0fd3c5b2._.js",
-  "static/chunks/node_modules_3ed5ba34._.js"
-],
-    source: "dynamic"
-});
